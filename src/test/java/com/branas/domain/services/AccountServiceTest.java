d1332119a26706e0b9d0d2ff8d280b83edc0dd3e
david <davidabreu.dev@gmail.com>
2023-09-13 13:09:02 -0300

    created AccountServiceTest

---
+++ src/test/java/com/branas/domain/services/AccountServiceTest.java
@@ -0,0 +1,25 @@
+package com.branas.domain.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class AccountServiceTest {
+
+    @Test
+    void sendEmail() {
+    }
+
+    @Test
+    void validateCpf() {
+    }
+
+    @Test
+    void signup() {
+    }
+
+    @Test
+    void getAccount() {
+    }
+}
