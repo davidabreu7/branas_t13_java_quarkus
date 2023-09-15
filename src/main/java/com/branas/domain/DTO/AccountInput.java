a7414a6fc2accdbf49f7be66347c17b70954bf55
David Abreu <62473543+davidabreu7@users.noreply.github.com>
2023-09-15 14:49:54 -0300

    Aula 1 uc1 (#1)
    * created AccountServiceTest
    
    * AccountServiceTest.ShouldCreatePassanger test passed
    
    * AccountServiceTest.ShouldCreatePassanger random factor
    
    * AccountServiceTest.ShouldCreatePassenger random factor
    
    * configured jacoco report

--- src/main/java/com/branas/domain/DTO/AccountInput.java
+++ src/main/java/com/branas/domain/DTO/AccountInput.java
@@ -5,4 +5,12 @@ public record AccountInput(String name,
                            String cpf,
                            String carPlate,
                            boolean isPassenger,
-                           boolean isDriver) {}
+                           boolean isDriver) {
+    public AccountInput(String name, String email, String cpf, boolean isPassenger) {
+        this(name, email, cpf, null, isPassenger, false);
+    }
+
+    public AccountInput(String name, String email, String cpf, String carPlate, boolean isDriver) {
+        this(name, email, cpf, carPlate, false, isDriver);
+    }
+}
